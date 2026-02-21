ca842309328d6ea3941352cbd857de893442c9e8
Kharlamov Vladislav <kharlamov.workspace@gmail.com>
2026-02-22 01:00:48 +0300

    Initial commit: Create basic project structure and define a goal

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    java
+    id("org.springframework.boot") version "4.0.2"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.itq"
+version = "0.0.1-SNAPSHOT"
+description = "itq-test-problem"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-liquibase")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("org.postgresql:postgresql")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-liquibase-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/test/java/com/itq/testproblem/ItqTestProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itq.testproblem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItqTestProblemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
